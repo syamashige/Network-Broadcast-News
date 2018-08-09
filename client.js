aaa17fb44d53cb9a87b5173a1bd2b31bcc967333
Sarah <yamashige.sarah@gmail.com>
2018-08-08 20:09:22 -1000

    linked the client

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 
-const client = net.createConnection(6968, "0.0.0.0", () => {
+const client = net.createConnection(6968, "54.218.81.62", () => {
     // client.write("WELCOME!");
     client.on("data", data => {
         console.log("\x1b[36m", data.toString());
